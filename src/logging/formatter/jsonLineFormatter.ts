bd4cd031790f3ce130d4220f003bbc61f21be756
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-03-27 16:56:07 -0400

    fix: export the new features

--- src/logging/formatter/jsonLineFormatter.ts
+++ src/logging/formatter/jsonLineFormatter.ts
@@ -7,31 +7,11 @@ import type { LoggerContext } from "../log-context/LoggerContext.js"
 export const jsonLineFormatter: TextFormatter = (logRecord) => {
   const { scopes, ...context } = logRecord.properties as LoggerContext
 
-  return formatConsoleMessage({
-    timestamp: logRecord.timestamp,
-    level: logRecord.level,
-    scopes,
-    // We have to cast because we use LogTape's types directly instead of providing our own adapters.
-    // LogTape supports template literals, but we don't.
-    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals to learn more about template literals.
-    // We don't validate for performance reasons. We compensate with tests.
-    message: logRecord.rawMessage as string,
-    context,
-  })
-}
-
-function formatConsoleMessage(data: {
-  timestamp: number
-  scopes: readonly string[]
-  level: string
-  message: string
-  context: Record<string, unknown>
-}): string {
   return JSON.stringify({
-    ...data.context,
-    timestamp: new Date(data.timestamp).toISOString(),
-    level: data.level.toUpperCase(),
-    scope: data.scopes.length > 0 ? data.scopes.join(" ") : undefined,
-    message: data.message,
+    ...context,
+    timestamp: new Date(logRecord.timestamp).toISOString(),
+    level: logRecord.level.toUpperCase(),
+    scope: scopes.length > 0 ? scopes.join(" ") : undefined,
+    message: logRecord.rawMessage,
   })
 }

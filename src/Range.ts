0987e6bbe224de803bc8649c29a739a475726b49
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-05-25 21:22:50 -0400

    fix: revert 18e39f9 because .exactOptional() exists (#3)

--- src/Range.ts
+++ src/Range.ts
@@ -35,7 +35,7 @@ export type InclusiveRange<TNumericType extends NumericType> = {
   /**
    * Optional outer range that this range must stay within.
    */
-  readonly limits?: Range<TNumericType> | undefined
+  readonly limits?: Range<TNumericType>
 }
 
 /**
@@ -66,7 +66,7 @@ export type ExclusiveRange<TNumericType extends NumericType> = {
   /**
    * Optional outer range that this range must stay within.
    */
-  readonly limits?: Range<TNumericType> | undefined
+  readonly limits?: Range<TNumericType>
 }
 
 type NumericType = "float" | "integer"
